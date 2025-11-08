80a547fb778a3fa38c5769e2c760516b5c630bfe
Dennis Li <46885316+DennisLi66@users.noreply.github.com>
2025-11-07 18:48:10 -0800

    Added Schema for Past Partners

--- studio-new_website/schemaTypes/index.ts
+++ studio-new_website/schemaTypes/index.ts
@@ -1,10 +1,12 @@
 import {postType} from './postType'
 import boardMember from './boardMember'
 import aboutUs from "./aboutUs"
+import partnerPast from "./aboutUsPartnerPast"
 
 export const schemaTypes = 
 [
     boardMember,
     postType,
-    aboutUs
+    aboutUs,
+    partnerPast
 ]
